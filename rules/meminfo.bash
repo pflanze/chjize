7b15b6740ccdc8f9a665a0079afd49f5606647d5
Christian Jaeger <ch@christianjaeger.ch>
2021-02-17 15:17:07 +0000

    rules/meminfo.bash: re-read each time
    Since I'm running `swapon -a` that would make it fail. Ever evil.

--- rules/meminfo.bash
+++ rules/meminfo.bash
@@ -1,9 +1,10 @@
 
-meminfo=$(cat /proc/meminfo)
-
 meminfo_get () {
     export FIELD=$1
     export UNIT=$2
+    set -eu
+    local meminfo
+    meminfo=$(cat /proc/meminfo)
     echo "$meminfo" | perl -wne \
       '/^\Q$ENV{FIELD}\E:\s*(\d+)\s*$ENV{UNIT}/ and push @vs, $1; 
        END { @vs == 1 or die "found ".@vs." values not 1"; print @vs, "\n" or die }'
