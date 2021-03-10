fa09775df7543a1345ae08bf47cd96ae0924c744
Christian Jaeger <ch@christianjaeger.ch>
2021-03-10 01:27:42 +0000

    targets.mk: use a default recipe

--- targets.mk
+++ targets.mk
@@ -1,4 +1,9 @@
 
+# The default recipe: take the action with the same name as the
+# target, with no arguments.
+% ::
+	sbin/action $@
+
 # Backspace is being processed differently on Cygwin compared to
 # Linux, this is trying to abstract it away:
 BS=$(shell sbin/BS)
@@ -57,7 +62,6 @@ README.md: chj-bin .targets docstrings graph.svg
 	sbin/update-readme README.md < .targets
 
 install: README.md
-	sbin/action $@
 
 auto-update: README.md graph.svg install
 	git commit -m "auto-update" README.md graph.svg install || true
@@ -70,37 +74,29 @@ auto-update: README.md graph.svg install
 
 # Install dependencies to run the `graph` target.
 graph-deps: perhaps_aptupdate
-	sbin/action $@
 
 # Import cj-key.asc into the keyring of the current user.
 key:
-	sbin/action $@
 
 # Run `apt-get update` unless already run in the last 24 hours.
 perhaps_aptupdate:
-	sbin/action $@
 
 # Upgrade the system (via dist-upgrade), necessary on a fresh instance
 # on typical cloud hostings like Amazon's.
 debian_upgrade: perhaps_aptupdate
-	sbin/action $@
 
 # Install `rxvt-unicode` and trim it down for security and simplicity.
 urxvt: perhaps_aptupdate 
-	sbin/action $@
 
 # Install my preferred Debian packages.
 debianpackages: urxvt
-	sbin/action $@
 
 # Install `g++`.
 cplusplus: perhaps_aptupdate
-	sbin/action $@
 
 # Check out [git-sign](https://github.com/pflanze/git-sign); used by
 # most other targets.
 git-sign: key
-	sbin/action $@
 
 chj-perllib-checkout: .bs git-sign
 	bin/chj-checkout chj-perllib-checkout https://github.com/pflanze/chj-perllib.git perllib '^r($(BS)d+)$$'
@@ -109,7 +105,6 @@ chj-perllib-checkout: .bs git-sign
 # [chj-perllib](https://github.com/pflanze/chj-perllib). These depend
 # on `fperl` now, thus that is installed as well.
 chj-perllib: chj-perllib-checkout fperl
-	sbin/action $@
 # ^ XX would fperl-noinstall be enough? It would be preferable.
 
 # Install [chj-bin](https://github.com/pflanze/chj-bin).
@@ -122,11 +117,9 @@ chj-emacs-checkout: git-sign
 # Install [chj-emacs](https://github.com/pflanze/chj-emacs) in
 # /opt/chj/emacs/.
 chj-emacs: chj-emacs-checkout
-	sbin/action $@
 
 # Install GNU Emacs via APT.
 debian-emacs:
-	sbin/action $@
 
 # Install debian-emacs and chj-emacs targets.
 emacs: debian-emacs chj-emacs
@@ -148,13 +141,11 @@ cj-git-patchtool: debianpackages chj-bin git-sign
 
 # Automatically configure some (English and German speaking) locales.
 locales: chj-bin
-	sbin/action $@
 
 # Automatically configure debconf to be in Noninteractive mode (run
 # this to avoid other targets waiting for inputs; also, it will be the
 # only mode that works with -j2).
 debconf-noninteractive:
-	sbin/action $@
 
 # Check out the last tagged versions of various repositories into
 # `/opt/chj` (uses signed tags via git-sign to ensure you get what I
@@ -182,7 +173,6 @@ xfce-server: perhaps_aptupdate chj-bin dotconfig-xfce4-checkout
 	sbin/action $@ xfce server
 
 xfce4_load_profile: chj-bin xfce-server
-	sbin/action $@
 
 # Set up Debian so that a graphical login will read the `~/.profile`
 # file (which they stopped doing at some point, dunno why); currently
@@ -204,7 +194,6 @@ load_profile: xfce4_load_profile
 # other users, instead run `/opt/chj/chjize/bin/mod-user` as that user
 # (in its home dir)
 moduser: chj key
-	sbin/action $@
 
 # Install the [Functional Perl](http://functional-perl.org) library
 # and its dependencies. Currently installs dependencies only from
@@ -215,13 +204,11 @@ moduser: chj key
 # /opt/functional-perl/lib;`! For a full installation, use the `fperl`
 # target.
 fperl-noinstall: git-sign
-	sbin/action $@
 
 # This is the `fperl-noinstall` target but also *does* run `make
 # install`. (This still does not access CPAN, and thus is still
 # secure.)
 fperl: fperl-noinstall
-	sbin/action $@
 
 
 gambit-checkout: .bs git-sign
@@ -229,38 +216,31 @@ gambit-checkout: .bs git-sign
 
 # Install a patched version of the Gambit Scheme system.
 gambit: gambit-checkout cplusplus debianpackages chj-bin chj-emacs virtualmem_3GB
-	sbin/action $@
 # ^ chj-emacs for /opt/chj/emacs/bin/gam-emacs
 
 # Install Qemu, and
 # [cj-qemucontrol](https://github.com/pflanze/cj-qemucontrol).
 qemu: git-sign gambit
-	sbin/action $@
 
 # Xfce4, desktop packages.
 slim-desktop: chj set-x-terminal-emulator xfce4_load_profile
-	sbin/action $@
 
 # `slim-desktop`, but also removes pulseaudio and installs jack, and
 # removes the login managers. Xfce4 has to be started via `startx`
 # from the console after this! (That latter part was a hack to work
 # around some issues in Debian stretch / get what I wanted.)
 full-desktop: slim-desktop
-	sbin/action $@
 
 # The `full-desktop` target but also runs `apt-get autoremove` to free
 # up the space taken by now unused packages.
 full-desktop_autoremove: full-desktop
-	sbin/action $@
 
 # Install and configure a local dns resolver (unbound).
 dnsresolver:
-	sbin/action $@
 
 # Install mercurial, and hg-fast-export from either Debian or upstream
 # source.
 mercurial: chj-bin
-	sbin/action $@
 
 # Ensure basic system readyness.
 system: debian_upgrade locales
@@ -270,23 +250,19 @@ system: debian_upgrade locales
 # setup. Currently assumes a single user will be used to run the VNC
 # server as (hard codes ports).
 slim-vncserver: perhaps_aptupdate slim-desktop chj-bin
-	sbin/action $@
 
 # Server with VNC and Xfce4 desktop plus common chj packages. Note the
 # message about finishing the setup.
 full-vncserver: slim-vncserver debianpackages system
-	sbin/action $@
 
 # Client side VNC setup.
 vncclient: perhaps_aptupdate
-	sbin/action $@
 
 
 # Create and activate (including adding to fstab) a swap file if none
 # is already active. Size is automatically chosen to be the same as
 # the RAM size or enough to give a total of RAM+swap of 3 GB.
 swap: chj-bin
-	sbin/action $@
 
 # Enable swap if there is less than 3 GB of RAM available. (Only
 # provides 3 GB of virtual memory if there is at least 1 GB of RAM!
@@ -294,7 +270,6 @@ swap: chj-bin
 # that more swap wouldn't be helpful anyway, so leave it at just what
 # the `swap` target provides.)
 virtualmem_3GB: chj-bin
-	sbin/action $@
 
 # Remove `sudo` (often provided by images) since it's a security
 # issue. Since this will lock you out from acting as root unless you
@@ -302,7 +277,6 @@ virtualmem_3GB: chj-bin
 # `SUDO_FORCE_REMOVE=yes` before running this target or it will
 # fail. If instead you want to keep `sudo` installed, set `NOSUDO=no`.
 nosudo:
-	sbin/action $@
 
 # Runs the `nosudo` target except it will force removal even without
 # `SUDO_FORCE_REMOVE=yes` if it can ensure that the root login can be
@@ -312,24 +286,19 @@ nosudo:
 # in via password instead of a key! Still is a NOP if `NOSUDO=no` is
 # set.
 nosudo-auto:
-	sbin/action $@
 
 # Set `x-terminal-emulator` in Debian's alternatives system to
 # `/opt/chj/bin/term`, which uses urxvt.
 set-x-terminal-emulator: urxvt chj-bin
-	sbin/action $@
 
 # Install Firefox from Debian.
 firefox: perhaps_aptupdate
-	sbin/action $@
 
 # Install unison from Debian (console version).
 unison: perhaps_aptupdate
-	sbin/action $@
 
 # Install guix from Debian. Upgrades system to Debian Bullseye!
 guix: perhaps_aptupdate bullseye
-	sbin/action $@
 
 # Create `schemen` user, copy ssh keys from root to it.
 schemen-user: moduser
@@ -338,7 +307,6 @@ schemen-user: moduser
 # Check out and build [lili](https://github.com/pflanze/lili) as the
 # `schemen` user.
 schemen-lili: schemen-user gambit chj-emacs
-	sbin/action $@
 # ^ chj-emacs for gam-emacs, that's already part of `gambit`, but
 # being explicit won't hurt. Full `emacs` is required by `schemen`.
 
@@ -347,24 +315,20 @@ schemen-lili: schemen-user gambit chj-emacs
 # on your desktop: `scp .vncclient-passwords/passwd
 # root@tmp:/opt/chj/chjize/tmp/`.
 schemen: tmp/passwd system full-vncserver nosudo-auto gambit emacs schemen-user schemen-lili firefox unison
-	sbin/action $@
 
 # Remove xorg and xserver-xorg packages. This is a horrible HACK for
 # cases where they should never be installed in the first place but I
 # can't figure out why they are. (This is called from within the
 # slim-desktop rule.)
 remove-xserver:
-	sbin/action $@
 
 
 # Changes /etc/apt/sources.list to point to bullseye instead of buster
 sources-bullseye: chj-bin
-	sbin/action $@
 
 # Upgrade a Debian system running Buster to Bullseye (does include
 # running `apt-get update` as part of the action).
 bullseye: sources-bullseye
-	sbin/action $@
 # (Depending on perhaps_aptupdate would be wrong, both because that
 # would not run if it ran within the last 24 hours, but also because
 # there's no way to specify it running *after* sources-bullseye.)
