47d4caf0dddf3a8f1bf03ff5a3bd26f953b25846
Christian Jaeger <ch@christianjaeger.ch>
2021-03-11 17:08:30 +0000

    Move more code to lib/root-add-ssh.bash

--- lib/root-add-ssh.bash
+++ lib/root-add-ssh.bash
@@ -40,3 +40,52 @@ uid_authorizedkeysfile () {
 }
 
 
+root_add_ssh_keys_from_file () {
+    local from
+    from=$1
+
+    local to
+    to=$(uid_authorizedkeysfile 0)
+
+    if [ "$from" = "$to" ]; then
+        echo "$0: odd, should not be identical, BUG?: '$from', '$to'"
+        # Well, unless both users were really configured to have
+        # the same dir, but that would be odd since
+        # insecure/voiding sudo anyway.
+        #do_nosudo_force # really?
+        false
+    else
+        local cleaned
+        cleaned=$(mktemp)
+
+        if [ -e "$to" ]; then
+            # Remove entries from AWS
+            01ok grep -v 'Please login as the user' < "$to" > "$cleaned"
+            # keep backup, all of them due to concurrency, use file's inode
+            # number for guaranteed conflict freedom
+            local ino
+            ino=$(stat --printf '%i' "$to")
+            ln "$to" "$to.$ino~" || true
+        fi
+
+        # Add entries from $from which are not in $cleaned
+        local tmp
+        tmp=$(mktemp)
+        {
+            cat "$from"
+            echo
+            cat "$cleaned"
+            echo
+        } > "$tmp"
+        local tmp2
+        tmp2=$(mktemp)
+        LANG=C sort -u < "$tmp" > "$tmp2"
+        local new
+        new=$(mktemp -p "$(dirname "$to")")
+        egrep -v '^$' < "$tmp2" > "$new"
+        # new is private, make it "normal"
+        chmod a+r "$new"
+        mv -f "$new" "$to"
+    fi
+}
+
