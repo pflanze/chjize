319403ad880be803ccb2b3b2d552db029987a81c
Christian Jaeger <ch@christianjaeger.ch>
2021-03-11 12:39:12 +0000

    aptupdate improvement part 3: wrap apt calls to do the updating

--- lib/apt-locked.bash
+++ lib/apt-locked.bash
@@ -4,6 +4,30 @@
 
 source /opt/chj/chjize/lib/remove-path.bash
 
+is_update () {
+    # true if any of the arguments are looking like it's doing an
+    # installation operation
+    local arg
+    for arg in "$@"; do
+        case "$arg" in
+            update | upgrade | dist-upgrade | dselect-upgrade \
+                | install | download | source)
+                return 0;;
+            *)
+            ;;
+        esac
+    done
+    return 1
+}
+
+conditional_apt_update () {
+    if is_update "$@"; then
+        # (XX could optimize this by defining this in a library
+        # instead of as a separate process: )
+        perhaps-aptupdate
+    fi
+}
+
 apt-locked() {
     local PATH=$PATH
     remove-path /opt/chj/chjize/sbin
