119b9bd704414644f84948666612534e6a9af353
Christian Jaeger <ch@christianjaeger.ch>
2021-11-16 15:12:32 +0000

    Add `chroot-desktop` target

--- targets.mk
+++ targets.mk
@@ -270,8 +270,13 @@ dnsmasq:
 # necessary permissions.
 qemu: cj-qemucontrol dnsmasq
 
+# Desktop things still needed in a chroot (via `chrootlogin` tool from
+# chj-bin) running inside a deskop which is installed on the host.
+chroot-desktop: system chj fonts set-x-terminal-emulator
+	touch chroot-desktop
+
 # Xfce4, desktop packages.
-slim-desktop: system chj fonts set-x-terminal-emulator xfce4_load_profile cj-unattended-upgrades-desktop firefox
+slim-desktop: chroot-desktop xfce4_load_profile cj-unattended-upgrades-desktop firefox
 
 # `slim-desktop`, but also removes pulseaudio and installs jack, and
 # removes the login managers. Xfce4 has to be started via `startx`
